259b5099f0352cce057efff9f61618304824da1c
NikitaPakhomov <pakhomov.nikita.v@gmail.com>
2020-12-14 18:18:54 +0700

    popup done

--- app/js/js_lesson2.js
+++ app/js/js_lesson2.js
@@ -7,10 +7,10 @@ let data = [
         "companions": `${randomInteger()}`,
         "decorative": `${randomInteger()}`,
         "service": `${randomInteger()}`,
-        "Nofear": `${randomInteger()}`,
-        "Shedslittle": `${randomInteger()}`,
-        "Excellenthealth": `${randomInteger()}`,
-        "Goodobedience": `${randomInteger()}`,
+        "nofear": `${randomInteger()}`,
+        "shedslittle": `${randomInteger()}`,
+        "excellenthealth": `${randomInteger()}`,
+        "goodobedience": `${randomInteger()}`,
         "devoted": `${randomInteger()}`
     },
     {
@@ -21,10 +21,10 @@ let data = [
         "companions": `${randomInteger()}`,
         "decorative": `${randomInteger()}`,
         "service": `${randomInteger()}`,
-        "Nofear": `${randomInteger()}`,
-        "Shedslittle": `${randomInteger()}`,
-        "Excellenthealth": `${randomInteger()}`,
-        "Goodobedience": `${randomInteger()}`,
+        "nofear": `${randomInteger()}`,
+        "shedslittle": `${randomInteger()}`,
+        "excellenthealth": `${randomInteger()}`,
+        "goodobedience": `${randomInteger()}`,
         "devoted": `${randomInteger()}`
     },
     {
@@ -35,10 +35,10 @@ let data = [
         "companions": `${randomInteger()}`,
         "decorative": `${randomInteger()}`,
         "service": `${randomInteger()}`,
-        "Nofear": `${randomInteger()}`,
-        "Shedslittle": `${randomInteger()}`,
-        "Excellenthealth": `${randomInteger()}`,
-        "Goodobedience": `${randomInteger()}`,
+        "nofear": `${randomInteger()}`,
+        "shedslittle": `${randomInteger()}`,
+        "excellenthealth": `${randomInteger()}`,
+        "goodobedience": `${randomInteger()}`,
         "devoted": `${randomInteger()}`
     },
     {
@@ -49,10 +49,10 @@ let data = [
         "companions": `${randomInteger()}`,
         "decorative": `${randomInteger()}`,
         "service": `${randomInteger()}`,
-        "Nofear": `${randomInteger()}`,
-        "Shedslittle": `${randomInteger()}`,
-        "Excellenthealth": `${randomInteger()}`,
-        "Goodobedience": `${randomInteger()}`,
+        "nofear": `${randomInteger()}`,
+        "shedslittle": `${randomInteger()}`,
+        "excellenthealth": `${randomInteger()}`,
+        "goodobedience": `${randomInteger()}`,
         "devoted": `${randomInteger()}`
     },
     {
@@ -63,10 +63,10 @@ let data = [
         "companions": `${randomInteger()}`,
         "decorative": `${randomInteger()}`,
         "service": `${randomInteger()}`,
-        "Nofear": `${randomInteger()}`,
-        "Shedslittle": `${randomInteger()}`,
-        "Excellenthealth": `${randomInteger()}`,
-        "Goodobedience": `${randomInteger()}`,
+        "nofear": `${randomInteger()}`,
+        "shedslittle": `${randomInteger()}`,
+        "excellenthealth": `${randomInteger()}`,
+        "goodobedience": `${randomInteger()}`,
         "devoted": `${randomInteger()}`
     },
     {
@@ -77,10 +77,10 @@ let data = [
         "companions": `${randomInteger()}`,
         "decorative": `${randomInteger()}`,
         "service": `${randomInteger()}`,
-        "Nofear": `${randomInteger()}`,
-        "Shedslittle": `${randomInteger()}`,
-        "Excellenthealth": `${randomInteger()}`,
-        "Goodobedience": `${randomInteger()}`,
+        "nofear": `${randomInteger()}`,
+        "shedslittle": `${randomInteger()}`,
+        "excellenthealth": `${randomInteger()}`,
+        "goodobedience": `${randomInteger()}`,
         "devoted": `${randomInteger()}`
     },
     {
@@ -91,28 +91,36 @@ let data = [
         "companions": `${randomInteger()}`,
         "decorative": `${randomInteger()}`,
         "service": `${randomInteger()}`,
-        "Nofear": `${randomInteger()}`,
-        "Shedslittle": `${randomInteger()}`,
-        "Excellenthealth": `${randomInteger()}`,
-        "Goodobedience": `${randomInteger()}`,
+        "nofear": `${randomInteger()}`,
+        "shedslittle": `${randomInteger()}`,
+        "excellenthealth": `${randomInteger()}`,
+        "goodobedience": `${randomInteger()}`,
         "devoted": `${randomInteger()}`
     }
 ]
 
 let itemsCont = document.querySelector('.cards');
 let itemTemplate = itemsCont.querySelector('#template');
-for (i = 0; i < 12; i++) {
-    itemsCont.appendChild(createElement(getInfo()));
+
+
+toHTML(12);
+
+function toHTML(count) {
+    for (i = 0; i < count; i++) {
+        let elem = createElement(getInfo());
+        itemsCont.appendChild(elem);
+    }
 }
 
-function createElement({ img, name, cost, hunt, companions, decorative, service, Nofear, Shedslittle, Excellenthealth, Goodobedience, devoted }) {
+
+function createElement({ img, name, cost, hunt, companions, decorative, service, nofear, shedslittle, excellenthealth, goodobedience, devoted }) {
     let elem = document.createElement('div');
     elem.classList.add('cards__item');
     elem.append(itemTemplate.content.cloneNode(true));
     elem.querySelector('.cards__img').src = `${img}`;
     elem.querySelector('.cards__name').textContent = `${name}`;
     elem.querySelector('.cards__cost').textContent = `${cost}`;
-    setData(elem, hunt, companions, decorative, service, Nofear, Shedslittle, Excellenthealth, Goodobedience, devoted);
+    setData(elem, hunt, companions, decorative, service, nofear, shedslittle, excellenthealth, goodobedience, devoted);
     return elem;
 
 }
@@ -131,10 +139,10 @@ function setData(elem, ...datas) {
     elem.dataset.companions = datas[1];
     elem.dataset.decorative = datas[2];
     elem.dataset.service = datas[3];
-    elem.dataset.Nofear = datas[4];
-    elem.dataset.Shedslittle = datas[5];
-    elem.dataset.Excellenthealth = datas[6];
-    elem.dataset.Goodobedience = datas[7];
+    elem.dataset.nofear = datas[4];
+    elem.dataset.shedslittle = datas[5];
+    elem.dataset.excellenthealth = datas[6];
+    elem.dataset.goodobedience = datas[7];
     elem.dataset.devoted = datas[8];
 }
 
@@ -143,19 +151,49 @@ const btnShow = document.querySelector('.filters__show-animals');
 const foundCount = document.querySelector('#filters__count');
 const filter = document.querySelector('.filters');
 const filtersPopup = document.querySelector('.filters__popup');
+let filterTimer;
+let filtersActive = [];
+
 
 
 
 filter.addEventListener("click", () => findClickTarget(event))
 
 function findClickTarget(e) {
-    if (e.target.className == "checkbox") {
-        let timer = setTimeout(() => insertPopup(e.target), 3000);
-    }
+    let target = e.target;
+    if (target.className == "checkbox") {
+        filtersPopup.classList.remove('filters__popup_active')
+        clearTimeout(filterTimer);
 
+        if (target.checked) {
+            filterTimer = setTimeout(() => insertPopup(target), 3000);
+            filtersActive.push(target);
+
+
+        } else {
+            filtersPopup.classList.remove('filters__popup_active')
+            if (filtersActive.includes(target)) {
+                console.log(filtersActive.indexOf(target));
+                console.log(target);
+                filtersActive.splice(filtersActive.indexOf(target), 1);
+
+            }
+            filterTimer = setTimeout(() => insertPopup(filtersActive[filtersActive.length - 1]), 3000);
+        }
+        console.log(filtersActive[0].dataset);
+    }
 }
 
 function insertPopup(target) {
-    filtersPopup.style.left = target.labels[0].getBoundingClientRect().width + 32 + 'px';
-    filtersPopup.style.top = target.labels[0].getBoundingClientRect().y - filter.getBoundingClientRect().y + "px";
+    if (target) {
+        filtersPopup.style.left = target.labels[0].getBoundingClientRect().width + 32 + 'px';
+        filtersPopup.style.top = target.labels[0].getBoundingClientRect().y - filter.getBoundingClientRect().y + "px";
+        if (!filtersPopup.classList.contains('filters__popup_active')) {
+            filtersPopup.classList.add('filters__popup_active');
+        }
+    }
 }
+
+//------------------filtering-------------------------
+
+
